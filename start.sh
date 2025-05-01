2bea5c063d17a299fddfe2aba948c7c940dda259
fanye6666 <50435071+fanye6666@users.noreply.github.com>
2025-05-01 20:44:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ NGINX="android-nginx"
 echo -e "\n 1.create ${REDROID}"
 docker run -itd --name=${REDROID} \
     --memory-swappiness=0 \
-    --privileged --pull always \
+    --privileged \
     -v ./redroid/data:/data \
     redroid/redroid:8.1.0-latest \
     androidboot.hardware=mt6891 ro.secure=0 ro.boot.hwc=GLOBAL    ro.ril.oem.imei=861503068361145 ro.ril.oem.imei1=861503068361145 ro.ril.oem.imei2=861503068361148 ro.ril.miui.imei0=861503068361148 ro.product.manufacturer=Xiaomi ro.build.product=chopin \
